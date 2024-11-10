a974d36b7a4f9cb98044f21220b924a7bcc19b7c
arroubi-youness <156539478+arroubi-youness@users.noreply.github.com>
2024-11-10 00:05:27 +0000

    add exception filter to register controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,42 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+ import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
- 
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
   @Post('register')
   async register(@Body() createUserDto: CreateUserDto) {
-    await this.authService.save_new(createUserDto);
-    return { message: 'User successfully created' };
+    try {
+      await this.authService.save_new(createUserDto);
+      return { message: 'User successfully created' };
+    } catch (error) {
+       if ((error as any).code === 11000) {  
+        throw new HttpException(
+          {
+            status: HttpStatus.CONFLICT,
+            error: 'User with this email already exists',
+          },
+          HttpStatus.CONFLICT,
+        );
+      }
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Error  during  the register  of  the  user',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
-
 }
